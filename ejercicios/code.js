bbca4677f4433c46db48ccdabf5cc5782550488e
Bmayo08 <142277228+Bmayo08@users.noreply.github.com>
2025-02-05 22:03:12 -0500

    equal and no equal

--- ejercicios/code.js
+++ ejercicios/code.js
@@ -62,3 +62,12 @@ console.log(luchador);
 curador.curar(luchador);
 luchador.pegar(curador);  
 
+// Configuración
+function testStrictNotEqual(val) {
+    if (val !==17) { // Cambia esta línea
+      return "Not Equal";
+    }
+    return "Equal";
+  }
+  
+  testStrictNotEqual(10);
