12e63770aa711d7f973261d366fcfb2a286db10c
Bmayo08 <142277228+Bmayo08@users.noreply.github.com>
2025-02-06 17:45:20 -0500

    ejercicio golf agregado

--- ejercicios/code.js
+++ ejercicios/code.js
@@ -72,5 +72,42 @@ function testStrictNotEqual(val) {
   }
   
   testStrictNotEqual(10);
+
+
+
+  //ejercicio de golf
   
+  const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+  // Cambia solo el código debajo de esta línea
+   
+  if(strokes ===1 ){
+     return "Hole-in-one!"
+  }else if(strokes <= par -2){
+   return "Eagle"
+  }else if(strokes === par -1){
+   return "Birdie"
+  } else if(strokes === par ){
+   return "Par"
+  }else if(strokes === par+1){
+   return"Bogey"
+  }else if(strokes === par +2 ){
+   return "Double Bogey"
+  }else if(strokes >= par+3){
+   return "Go Home!"
+  }else{
+   return "retirese";
+  }
+
+  // Cambia solo el código encima de esta línea
+}
+
+console.log(golfScore(5, 4)); 
+console.log(golfScore(5, 6)); 
+
+
+
+
   
+ 
