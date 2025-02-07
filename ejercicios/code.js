edbf282da66066e86d82ad9b24ea717a0b15660f
Bmayo08 <142277228+Bmayo08@users.noreply.github.com>
2025-02-06 20:03:22 -0500

    ejercicio blackjack añadido

--- ejercicios/code.js
+++ ejercicios/code.js
@@ -60,54 +60,100 @@ console.log(curador);
 console.log(luchador);
 
 curador.curar(luchador);
-luchador.pegar(curador);  
+luchador.pegar(curador);
 
 // Configuración
 function testStrictNotEqual(val) {
-    if (val !==10) { // Cambia esta línea
-      console.log(  "Not Equal");
-    }else
-    console.log( "Equal");
-    
-  }
-  
-  testStrictNotEqual(10);
+    if (val !== 10) { // Cambia esta línea
+        console.log("Not Equal");
+    } else
+        console.log("Equal");
 
-
-
-  //ejercicio de golf
-  
-  const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
-
-function golfScore(par, strokes) {
-  // Cambia solo el código debajo de esta línea
-   
-  if(strokes ===1 ){
-     return "Hole-in-one!"
-  }else if(strokes <= par -2){
-   return "Eagle"
-  }else if(strokes === par -1){
-   return "Birdie"
-  } else if(strokes === par ){
-   return "Par"
-  }else if(strokes === par+1){
-   return"Bogey"
-  }else if(strokes === par +2 ){
-   return "Double Bogey"
-  }else if(strokes >= par+3){
-   return "Go Home!"
-  }else{
-   return "retirese";
-  }
-
-  // Cambia solo el código encima de esta línea
 }
 
-console.log(golfScore(5, 4)); 
-console.log(golfScore(5, 6)); 
+testStrictNotEqual(10);
 
 
 
+//ejercicio de golf
 
-  
- 
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+    // Cambia solo el código debajo de esta línea
+
+    if (strokes === 1) {
+        return "Hole-in-one!"
+    } else if (strokes <= par - 2) {
+        return "Eagle"
+    } else if (strokes === par - 1) {
+        return "Birdie"
+    } else if (strokes === par) {
+        return "Par"
+    } else if (strokes === par + 1) {
+        return "Bogey"
+    } else if (strokes === par + 2) {
+        return "Double Bogey"
+    } else if (strokes >= par + 3) {
+        return "Go Home!"
+    } else {
+        return "retirese";
+    }
+
+    // Cambia solo el código encima de esta línea
+}
+
+console.log(golfScore(5, 4));
+console.log(golfScore(5, 6));
+
+// Conteo de cartas
+// En el juego de casino Blackjack, un jugador puede determinar si
+//  tiene una ventaja en la siguiente mano sobre la casa llevando la cuenta del número relativo de cartas altas
+//  y bajas que quedan en la baraja.Esto se llama cuenta de tarjetas.
+
+// Tener más cartas altas en la baraja es una ventaja para el jugador.
+// Se le asigna un valor a cada carta de acuerdo a la siguiente tabla.
+// Cuando el conteo es positivo, el jugador debería apostar alto.Cuando el conteo da 0 o negativo,
+//  el jugador debería apostar bajo.
+
+// Cambios del conteo	Cartas
+//     + 1	2, 3, 4, 5, 6
+// 0	7, 8, 9
+//     - 1	10, 'J', 'Q', 'K', 'A'
+// Escribirás una función para contar cartas.
+// Recibirá un parámetro card(carta) que puede ser un número o una cadena y aumentar o reducir la variable global count
+// (conteo) de acuerdo al valor de la carta(observa la tabla).La función devolverá una cadena con el conteo actual 
+// y la cadena Bet(Apuesta) si el conteo es positivo, o Hold(Espera) si el conteo es cero o negativo.El conteo actual
+//  y la decisión del jugador(Bet o Hold) deben estar separados por un solo espacio.
+
+
+let count = 0;
+
+function cc(card) {
+
+    switch (card) {
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+            count++;
+            break;
+        case 10:
+        case "J":
+        case "Q":
+        case "K":
+        case "A":
+            count--;
+            break;
+    }
+    if (count > 0) {
+        return count + "Bet"
+    } else {
+        return count + "Hold"
+    }
+
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
+
