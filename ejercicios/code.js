88bce2a6cf34cab59907a9827fccda973e389a23
Bmayo08 <142277228+Bmayo08@users.noreply.github.com>
2025-02-08 19:17:02 -0500

    ejercicio objetos añadido

--- ejercicios/code.js
+++ ejercicios/code.js
@@ -76,6 +76,8 @@ testStrictNotEqual(10);
 
 
 //ejercicio de golf
+// par numero establecido para llegar al hoyo
+// strokes golpes realizados 
 
 const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
 
@@ -157,3 +159,50 @@ function cc(card) {
 
 cc(2); cc(3); cc(7); cc('K'); cc('A');
 
+
+
+// Configuración
+const recordCollection = {
+    2548: {
+      albumTitle: 'Slippery When Wet',
+      artist: 'Bon Jovi',
+      tracks: ['Let It Rock', 'You Give Love a Bad Name']
+    },
+    2468: {
+      albumTitle: '1999',
+      artist: 'Prince',
+      tracks: ['1999', 'Little Red Corvette']
+    },
+    1245: {
+      artist: 'Robert Palmer',
+      tracks: []
+    },
+    5439: {
+      albumTitle: 'ABBA Gold'
+    }
+  };
+   
+//   1. Después de updateRecords(recordCollection, 5439, "artist", "ABBA"), artist debe ser la cadena ABBA
+// Esperando:2. Después de updateRecords(recordCollection, 5439, "tracks", "Take a Chance on Me"), tracks debe tener la cadena Take a Chance on Me como último y único elemento.
+// Esperando:3. Después de updateRecords(recordCollection, 2548, "artist", ""), artist no debe establecerse
+// Esperando:4. Después de updateRecords(recordCollection, 1245, "tracks", "Addicted to Love"), tracks debe tener la cadena Addicted to Love como último elemento.
+// Esperando:5. Después de updateRecords(recordCollection, 2468, "tracks", "Free"), tracks debe tener la cadena 1999 como primer elemento.
+// Esperando:6. Después de updateRecords(recordCollection, 2548, "tracks", ""), tracks no debe establecerse
+// Esperando:7. Después de updateRecords(recordCollection, 1245, "albumTitle", "Riptide"), albumTitle debe ser la cadena Riptide
+  // Cambia solo el código debajo de esta línea
+  function updateRecords(records, id, prop, value) {
+    const album = records[id];
+   
+    if (value === "") {
+      delete album[prop];
+    } else if (prop !== "tracks") {
+      album[prop] = value;
+    } else {
+      album["tracks"] = album["tracks"] || [];
+      album["tracks"].push(value);
+    }
+    // Return the full collection
+    return records;
+  }
+  
+  updateRecords(recordCollection, 5439, 'artist', 'ABBA');
