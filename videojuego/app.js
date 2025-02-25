ccf95b8ffe414437d4a25028417c3bd1de315258
Bmayo08 <142277228+Bmayo08@users.noreply.github.com>
2025-02-24 23:33:25 -0500

    actulizacion del juego

--- videojuego/app.js
+++ videojuego/app.js
@@ -1,3 +1,4 @@
+// variables locales
 let xp = 0;
 let health = 100;
 let gold = 50;
@@ -6,6 +7,7 @@ let fighting;
 let monsterHealth;
 let inventory = ["stick"];
 
+// variables del dom
 const button1 = document.querySelector('#button1');
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
@@ -16,12 +18,16 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
+
+// array de las armas
 const weapons = [
   { name: 'stick', power: 5 },
   { name: 'dagger', power: 30 },
   { name: 'claw hammer', power: 50 },
   { name: 'sword', power: 100 }
 ];
+
+// array de los monstruos
 const monsters = [
   {
     name: "slime",
@@ -39,6 +45,8 @@ const monsters = [
     health: 300
   }
 ]
+
+// array de las localizaciones
 const locations = [
   {
     name: "town square",
@@ -91,11 +99,13 @@ const locations = [
   }
 ];
 
-// initialize buttons
+// initializacion buttons
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 
+
+// actualizar los botones
 function update(location) {
   monsterStats.style.display = "none";
   button1.innerText = location["button text"][0];
@@ -119,6 +129,8 @@ function goCave() {
   update(locations[2]);
 }
 
+
+// comprar vida
 function buyHealth() {
   if (gold >= 10) {
     gold -= 10;
@@ -130,6 +142,7 @@ function buyHealth() {
   }
 }
 
+// comprar arma
 function buyWeapon() {
   if (currentWeapon < weapons.length - 1) {
     if (gold >= 30) {
@@ -149,7 +162,7 @@ function buyWeapon() {
     button2.onclick = sellWeapon;
   }
 }
-
+  // vender arma
 function sellWeapon() {
   if (inventory.length > 1) {
     gold += 15;
@@ -161,7 +174,7 @@ function sellWeapon() {
     text.innerText = "Don't sell your only weapon!";
   }
 }
-
+// funcion de luchar 
 function fightSlime() {
   fighting = 0;
   goFight();
@@ -185,6 +198,7 @@ function goFight() {
   monsterHealthText.innerText = monsterHealth;
 }
 
+// funcion para atacar
 function attack() {
   text.innerText = "The " + monsters[fighting].name + " attacks.";
   text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
@@ -210,7 +224,7 @@ function attack() {
     currentWeapon--;
   }
 }
-
+// ataque de monstruos
 function getMonsterAttackValue(level) {
   const hit = (level * 5) - (Math.floor(Math.random() * xp));
   console.log(hit);
@@ -224,7 +238,7 @@ function isMonsterHit() {
 function dodge() {
   text.innerText = "You dodge the attack from the " + monsters[fighting].name;
 }
-
+// derrotar monstruos
 function defeatMonster() {
   gold += Math.floor(monsters[fighting].level * 6.7);
   xp += monsters[fighting].level;
@@ -232,7 +246,7 @@ function defeatMonster() {
   xpText.innerText = xp;
   update(locations[4]);
 }
-
+// perder o ganar
 function lose() {
   update(locations[5]);
 }
@@ -241,6 +255,7 @@ function winGame() {
   update(locations[6]);
 }
 
+// reiniciar juego
 function restart() {
   xp = 0;
   health = 100;
@@ -252,7 +267,8 @@ function restart() {
   xpText.innerText = xp;
   goTown();
 }
-
+ 
+// juego de adivinar
 function easterEgg() {
   update(locations[7]);
 }
